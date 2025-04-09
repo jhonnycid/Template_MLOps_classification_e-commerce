c93c38bfc476a929b0fed52e1532c00b52391179
Jhonny Dominguez <jhonny.dominguez@gmail.com>
2025-04-09 16:37:02 +0200

    final_files

--- airflow/dags/ETL_model_train_dag.py
+++ airflow/dags/ETL_model_train_dag.py
@@ -17,7 +17,6 @@ with DAG(
         auto_remove=True,
         command='python3 data_processing.py',
         mount_tmp_dir=False,
-        #mounts=[Mount(source="C:/Users/jhonn/data", target="/app/data", type="bind")],
         mounts=[Mount(source="C:/Users/jhonn/Projets_DataScientest/Projet MLOPs/Template_MLOps_classification_e-commerce/airflow/data", target="/app/data", type="bind")],
         docker_url="tcp://host.docker.internal:2375",  # for windows
         network_mode="bridge")
@@ -28,7 +27,6 @@ with DAG(
         auto_remove=True,
         command='python3 train_model_v2.py',
         mount_tmp_dir=False,
-        #mounts=[Mount(source="C:/Users/jhonn/data", target="/app/data", type="bind")],
         mounts=[Mount(source="C:/Users/jhonn/Projets_DataScientest/Projet MLOPs/Template_MLOps_classification_e-commerce/airflow/data", target="/app/data", type="bind")],
         docker_url="tcp://host.docker.internal:2375",  # for windows
         network_mode="bridge")
